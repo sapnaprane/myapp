4288a17b5e0181b9909c022a0973314484626209
sapnaprane <136076086+sapnaprane@users.noreply.github.com>
2023-10-23 09:04:13 +0000

    update views

--- p4napp/p4napp/urls.py
+++ p4napp/p4napp/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 # importing views from views..py
 from .views import p4n_view
-
+from myapp import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('time/',p4n_view),
-    path('hello/', p4n_view, name='p4n_view')
+    # path('hello/', p4n_view, name='p4n_view')
+    path('myapp/', views.index, name='index'),
 ]
